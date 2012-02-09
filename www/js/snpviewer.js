d1b99b805e74f7ce49611879e60dfe3211b75b0b
Hiren Joshi <hirenj@gmail.com>
2012-02-09 18:36:41 +0100

    Switch on display of the CDS sequences
    Enable loading up of the CDS sequences

--- www/js/snpviewer.js
+++ www/js/snpviewer.js
@@ -219,6 +219,16 @@ jQuery(document).ready(function() {
 
             }
             
+            MASCP.cloneService(MASCP.TairReader,'CdsReader');
+
+            (new MASCP.CdsReader(null,'http://localhost:3000/data/latest/cds')).retrieve(agi,function() {
+                MASCP.registerLayer('cds',{'fullname':'CDS','color':'#ff00ff'});
+                MASCP.renderer.renderTextTrack('cds',this.result.getSequence().slice(0,-3));
+                MASCP.renderer.trackOrder.push('cds');
+                MASCP.renderer.showLayer('cds');
+                MASCP.renderer.refresh();
+            });
+
             setTimeout(function() {
                 jQuery('#agi').focus();            
             },1000);
