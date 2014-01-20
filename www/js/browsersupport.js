4bce6738d6bb059d100ce41d26a96b3a5a1ac953
Hiren Joshi <hirenj@gmail.com>
2014-01-20 09:47:06 +0100

    Fixes for Internet Explorers

--- www/js/browsersupport.js
+++ www/js/browsersupport.js
@@ -1,13 +1,23 @@
 (function(parent) {
 
 	if (parent.IE && parent.IE < 9) {
-		var message = "Your browser is not fully supported, functionality may be missing from the tool";
+		var message = "Your browser is not fully supported, data and functionality may be missing from the tool";
 		if (window.notify) {
 			window.notify.alert(message);
 		} else {
 			window.alert(message);
 		}
-		return;		
+		return;
 	}
+	window.addEventListener('load',function() {
+		if ( typeof JSandbox == 'undefined' ) {
+			var message = "Your browser is not fully supported, data and functionality may be missing from the tool";
+			if (window.notify) {
+				window.notify.alert(message);
+			} else {
+				window.alert(message);
+			}
+		}
+	});
 
 })(window);
