49a710c6293d6bcf7fc2052e341418aa300b92f3
Hiren Joshi <hirenj@gmail.com>
2015-09-18 14:45:14 +0200

    Add better notification

--- www/js/domaintool.js
+++ www/js/domaintool.js
@@ -2595,6 +2595,7 @@
 
       if (state.ids) {
         var watching = [];
+        var last_notification = null;
         state.ids.forEach(function(doc_id) {
           (new MASCP.GoogledataReader()).getMimetype(doc_id,function(err,type,title,extension) {
             if ( ! err ) {
@@ -2602,9 +2603,16 @@
                 watching.unshift(doc_id);
                 var run_watch = function() {
                   var to_remove = watching[0];
+                  if (last_notification !== null) {
+                    last_notification.hide();
+                  }
+                  last_notification = window.notify.info("Attempting to load "+title);
                   get_preferences().watchFile(watching[0],function(err,loaded) {
                     if (err) {
-                      window.notify.alert(err.message);
+                      if (last_notification !== null) {
+                        last_notification.hide();
+                      }
+                      last_notification = window.notify.alert(err.message);
                       setTimeout( run_watch, 500);
                       return;
                     }
@@ -2612,9 +2620,17 @@
                     if (watch_idx > -1) {
                       watching.splice(watch_idx,1);
                     }
-                    window.notify.info("Successfully loaded "+(loaded || "")).hideLater(500);
+                    if (last_notification !== null) {
+                      last_notification.hide();
+                    }
+                    last_notification = window.notify.info("Successfully loaded "+(loaded || ""));
                     if (watching.length > 0) {
                       setTimeout( run_watch, 100);
+                    } else {
+                      if (last_notification !== null) {
+                        last_notification.hide();
+                      }
+                      window.notify.info("Successfully loaded all data");
                     }
                   });
                 };
