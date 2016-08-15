5953be781346304eddfffe2c7f8d31653ea6bef9
Hiren Joshi <hirenj@gmail.com>
2016-08-15 23:37:32 +0200

    More pruning of unnecessary stuff

--- www/js/domaintool.js
+++ www/js/domaintool.js
@@ -1,47 +1,5 @@
     if (!(window.console && console.log)) { (function() { var noop = function() {}; var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn']; var length = methods.length; var console = window.console = {}; while (length--) { console[methods[length]] = noop; } }()); }
 
-
-    if (!Element.prototype.scrollIntoViewIfNeeded) {
-      Element.prototype.scrollIntoViewIfNeeded = function (centerIfNeeded) {
-        centerIfNeeded = arguments.length === 0 ? true : !!centerIfNeeded;
-
-        var parent = this.parentNode,
-          parentComputedStyle = window.getComputedStyle(parent, null),
-          parentBorderTopWidth = parseInt(parentComputedStyle.getPropertyValue('border-top-width')),
-          parentBorderLeftWidth = parseInt(parentComputedStyle.getPropertyValue('border-left-width')),
-          overTop = this.offsetTop - parent.offsetTop < parent.scrollTop,
-          overBottom = (this.offsetTop - parent.offsetTop + this.clientHeight - parentBorderTopWidth) > (parent.scrollTop + parent.clientHeight),
-          overLeft = this.offsetLeft - parent.offsetLeft < parent.scrollLeft,
-          overRight = (this.offsetLeft - parent.offsetLeft + this.clientWidth - parentBorderLeftWidth) > (parent.scrollLeft + parent.clientWidth);
-
-        if (centerIfNeeded) {
-          if (overTop || overBottom) {
-            parent.scrollTop = this.offsetTop - parent.offsetTop - parent.clientHeight / 2 - parentBorderTopWidth + this.clientHeight / 2;
-          }
-
-          if (overLeft || overRight) {
-            parent.scrollLeft = this.offsetLeft - parent.offsetLeft - parent.clientWidth / 2 - parentBorderLeftWidth + this.clientWidth / 2;
-          }
-        }
-        else {
-          if (overTop) {
-            parent.scrollTop = this.offsetTop - parent.offsetTop - parentBorderTopWidth;
-          }
-
-          if (overBottom) {
-            parent.scrollTop = this.offsetTop - parent.offsetTop - parentBorderTopWidth - parent.clientHeight + this.clientHeight;
-          }
-
-          if (overLeft) {
-            parent.scrollLeft = this.offsetLeft - parent.offsetLeft - parentBorderLeftWidth;
-          }
-
-          if (overRight) {
-            parent.scrollLeft = this.offsetLeft - parent.offsetLeft - parentBorderLeftWidth - parent.clientWidth + this.clientWidth;
-          }
-        }
-     };
-    }
     var DomaintoolPreferences = function() {
       this.waiting = [];
     };
@@ -759,7 +717,7 @@
             }
             if (document.getElementById('prot_'+uniprot.toLowerCase())) {
               document.getElementById('prot_'+uniprot.toLowerCase()).click();
-              document.getElementById('prot_'+uniprot.toLowerCase()).scrollIntoViewIfNeeded(true);
+              document.getElementById('prot_'+uniprot.toLowerCase()).scrollIntoView(true);
               return;
             } else {
               var selected = (document.getElementsByClassName('selected') || [])[0];
@@ -1063,7 +1021,7 @@
         }
         if (to_select) {
           to_select.click();
-          to_select.scrollIntoViewIfNeeded(true);
+          to_select.scrollIntoView(true);
         }
       },false);
     };
@@ -1175,7 +1133,7 @@
 
         });
         if (window.document.getElementsByClassName('selected').length > 0) {
-          window.document.getElementsByClassName('selected')[0].scrollIntoViewIfNeeded(true);
+          window.document.getElementsByClassName('selected')[0].scrollIntoView(true);
         }
     };
 
