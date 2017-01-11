bbd63b1393c3828c594efebfa6d514f23160b6a5
Hiren Joshi <hirenj@gmail.com>
2017-01-11 12:19:36 +0100

    Add in support for phospho

--- www/msdata.packed_homology.renderer.js
+++ www/msdata.packed_homology.renderer.js
@@ -70,6 +70,11 @@ var render_peptide = function(peptide) {
 		if (composition === 'Hex') {
 			composition = 'man';
 		}
+
+		if (composition === 'Phospho') {
+			composition = 'phospho';
+		}
+
 		if (composition.toLowerCase() == 'glcnac(b1-4)glcnac') {
 			composition = composition.toLowerCase();
 		}
