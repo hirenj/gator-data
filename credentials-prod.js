ed69c63ef61fcb730ea7b030221cc9b2b62f7828
Hiren Joshi <hirenj@gmail.com>
2017-01-23 23:18:31 +0100

    Add in production credentials

---
+++ credentials-prod.js
@@ -0,0 +1,8 @@
+
+if ( ! window.MASCP ) {
+  window.MASCP = {};
+}
+
+MASCP.AUTH0_CLIENT_ID='fNED1UGvPaP0XlrcEvWsHXIODIKy6WVB';
+MASCP.AUTH0_DOMAIN='glycocode.auth0.com';
+MASCP.AUTH0_SCOPES='openid name email';
