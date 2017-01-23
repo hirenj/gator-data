a7f417f2d409551345fe19c1770dd36d9d4b4b9a
Hiren Joshi <hirenj@gmail.com>
2017-01-23 22:57:22 +0100

    Parameterise auth0 settings

--- www/credentials.js
+++ www/credentials.js
@@ -4,5 +4,9 @@
 // MASCP.GOOGLE_SCOPES="openid profile email https://www.googleapis.com/auth/drive.install https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/drive https://spreadsheets.google.com/feeds/";
 
 // Beta scopes
-MASCP.GOOGLE_CLIENT_ID="979764834319-753mqsahj8kqu0qi61hhp5mu02f8br0v.apps.googleusercontent.com";
-MASCP.GOOGLE_SCOPES="openid profile email https://www.googleapis.com/auth/drive.appdata";
+// MASCP.GOOGLE_CLIENT_ID="979764834319-753mqsahj8kqu0qi61hhp5mu02f8br0v.apps.googleusercontent.com";
+// MASCP.GOOGLE_SCOPES="openid profile email https://www.googleapis.com/auth/drive.appdata";
+
+MASCP.AUTH0_CLIENT_ID='c836UTr1RTWn3qxGNm5QiuP7ogSlGNrp';
+MASCP.AUTH0_DOMAIN='hirenj.auth0.com';
+MASCP.AUTH0_SCOPES='openid name email';
